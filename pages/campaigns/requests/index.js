7428b4a7b9302491b10e88cc318c4f349f09834b
Andrew Christie <awchristie86@gmail.com>
2022-02-11 15:01:36 -0500

    finished

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,21 +1,67 @@
 import { Link } from "../../../routes";
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((e, i) => {
+          return campaign.methods.requests(i).call();
+        })
+    );
+    return { address, requests, requestCount, approversCount };
+  }
+
+  renderRows() {
+    return this.props.requests.map((req, idx) => {
+      return (
+        <RequestRow
+          id={idx}
+          request={req}
+          key={idx}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button></Button>
+            <Button primary floated="right" style={{ marginBottom: "5px" }}>
+              Create New Request
+            </Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
+        <div> Found {this.props.requestCount} requests. </div>
       </Layout>
     );
   }
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -10,6 +10,8 @@ class RequestsNew extends Component {
     value: "",
     description: "",
     recipient: "",
+    loading: false,
+    errorMessage: "",
   };
   static async getInitialProps(props) {
     const { address } = props.query;
@@ -20,6 +22,8 @@ class RequestsNew extends Component {
   onSubmit = async (e) => {
     e.preventDefault();
 
+    this.setState({ loading: true, errorMessage: "" });
+
     const campaign = Campaign(this.props.address);
     const { description, value, recipient } = this.state;
 
@@ -28,13 +32,21 @@ class RequestsNew extends Component {
       await campaign.methods
         .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
         .send({ from: accounts[0] });
-    } catch (err) {}
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>Back</a>
+        </Link>
         <h3>Create a Request</h3>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -56,7 +68,10 @@ class RequestsNew extends Component {
               onChange={(e) => this.setState({ value: e.target.value })}
             />
           </Form.Field>
-          <Button primary>Create</Button>
+          <Message error header="Oops" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create
+          </Button>
         </Form>
       </Layout>
     );
